15d073a4021f76b30be7fffc7c7108173b99523f
Daniel Collins <daniel.e.collins1@gmail.com>
2017-03-02 19:57:27 -0800

    Example printing

--- src/cannyEdgeDetector.cpp
+++ src/cannyEdgeDetector.cpp
@@ -23,6 +23,12 @@ void CannyEdgeDetector::detect_edges(bool serial)
     std::cout << "in canny edge detector" << std::endl;
     pixel_t *raw_pixels = m_image_mgr->getPixelHandle();
     unsigned input_pixel_length = m_image_mgr->getPixelCount();
+
+    /* test */
+    for (unsigned i = 0; i < input_pixel_length; i++) {
+        std::cout << "Pixels[" << std::dec << i << "].red : " << std::hex << (unsigned) raw_pixels[i].red << std::endl;
+    }
+
     if (true == serial) {
         std::cout << "  executing serially" << std::endl;
         pixel_t *buf0 = new pixel_t[input_pixel_length];
@@ -37,6 +43,9 @@ void CannyEdgeDetector::detect_edges(bool serial)
         //apply_hysteresis(pixel_t *out_pixels, pixel_t *in_pixels, pixel_t hi_thld, pixel_t lo_thld);
 
         memcpy(raw_pixels, buf0, input_pixel_length * sizeof(pixel_t[0]));
+
+        delete [] buf0;
+        delete [] buf1;
 
     } else { // GPGPU
         /* Copy pixels to device - results of each stage stored on GPU and passed to next kernel */
