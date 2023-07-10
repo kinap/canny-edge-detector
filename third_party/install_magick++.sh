acf86aa3017c010c3d68d7f4820a11527c456087
qyzhizi <2830942138@qq.com>
2023-07-10 15:10:33 +0800

    update magick lib installation

--- third_party/install_magick++.sh
+++ third_party/install_magick++.sh
@@ -7,10 +7,11 @@ mkdir -p $TOP/usr
 # download library
 cd $TOP/src
 curl -L https://www.imagemagick.org/download/ImageMagick.tar.gz | tar zx
+# curl -L https://github.com/ImageMagick/ImageMagick6/archive/refs/tags/6.9.12-90.tar.gz | tar zx
 
 # configure
 cd $(ls -d */ | head -n 1)
-./configure --prefix=$TOP/usr
+./configure --prefix=$TOP/usr --with-png=yes --with-jpeg=yes --with-jxl=yes --with-modules --with-lcms --with-xml --without-x
 
 # build
 make 
